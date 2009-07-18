930ecfa4134eabee07b191ecb6c90746027f8742
Cyril Mougel <cyril.mougel@gmail.com>
2009-07-18 17:15:04 +0200

    use a config file to define the nanite mapper

--- merb_app/config/init.rb
+++ merb_app/config/init.rb
@@ -12,6 +12,7 @@ Merb::Config.use do |c|
   # cookie session store configuration
   c[:session_secret_key]  = '6f29bcb20c69d0d74c786a6bab7caabd7f41f4d0'  # required for cookie session store
   c[:session_id_key] = '_merb_app_session_id' # cookie session id key, defaults to "_session_id"
+  c[:adapter] = 'thin'
 end
  
 Merb::BootLoader.before_app_loads do
@@ -24,6 +25,7 @@ Merb::BootLoader.after_app_loads do
     until EM.reactor_running?
       sleep 1
     end
-    Nanite.start_mapper(:host => 'localhost', :user => 'nanite', :pass => 'testing', :vhost => '/nanite', :log_level => 'info')
+    conf = YAML::load_file(Merb.root + '/config/nanite_config.yml')
+    Nanite.start_mapper(:host => conf[:host], :user => conf[:user], :pass => conf[:pass], :vhost => conf[:vhost], :log_level => conf[:log_level])
   end
 end
