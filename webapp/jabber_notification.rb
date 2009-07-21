4dc2c51d334894cd6a17cb5bebf5ef9dc1f7236d
Cyril Mougel <cyril.mougel@gmail.com>
2009-07-21 10:24:24 +0200

    Revert "starting using sinatra-authentification. My own version only edge"
    All migrate in merb_app. Now sinatra app have no authentication
    
    This reverts commit d200e182b4fc0124ec32d596e2fc7c575f3b90dd.

--- webapp/jabber_notification.rb
+++ webapp/jabber_notification.rb
@@ -1,17 +1,49 @@
+require 'rubygems'
 require 'sinatra'
 require 'nanite'
 require 'haml'
 require 'yaml'
 
 require 'couchrest'
+
+
 SERVER = CouchRest.new
 SERVER.default_database = 'jabber-notification'
 
-require File.dirname(__FILE__) + '/models/user.rb'
-require 'sinatra-authentication'
+class User < CouchRest::ExtendedDocument
+  use_database SERVER.default_database
 
-use Rack::Session::Cookie, :secret => 'A1 sauce 1s so good you should use 1t on a11 yr st34ksssss'
+  property :email
+  property :hashed_password
+  property :salt
 
+  view_by :email
+
+  attr_accessor :password, :password_confirmation
+
+  timestamps!
+
+  def password=(pass)
+    @password = pass
+    self.salt = User.random_string(10) if !self.salt
+    self.hashed_password = User.encrypt(@password, self.salt)
+  end
+
+  private
+
+  def self.encrypt(pass, salt)
+    Digest::SHA1.hexdigest(pass+salt)
+  end
+
+  def self.random_string(len)
+    #generate a random password consisting of strings and digits
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    newpass = ""
+    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+    return newpass
+  end
+
+end
 
 configure do
   #Starting mapper to nanite agent
@@ -26,7 +58,6 @@ configure do
 end
 
 get '/' do
-  login_required
   haml :index
 end
 
@@ -35,12 +66,52 @@ post '/jabber' do
   redirect '/'
 end
 
+get '/signup' do
+  haml :signup
+end
+
+post '/signup' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user] = @user.id
+    redirect '/'
+  else
+    session[:flash] = "failure!"
+    redirect '/'
+  end
+end
+
+get '/users' do
+  @users = User.all
+  haml :users
+end
+
 __END__
 
 @@layout
 %html
   %body
     = yield
+
+@@users
+%h1 Users
+%ul
+  - @users.each do |user|
+    %li= user.email
+
+@@signup
+%h1 Signup
+%form{:action => "/signup", :method => "post"}
+  %input{ :id => "user_email", :name => "user[email]", :size => 30, :type => "text" }
+  email
+  %br
+  %input{ :id => "user_password", :name => "user[password]", :size => 30, :type => "password" }
+  password
+  %br
+  %input{ :id => "user_password_confirmation", :name => "user[password_confirmation]", :size => 30, :type => "password" }
+  confirm
+  %br
+  %input{ :value => "sign up", :type => "submit" }
 
 @@index
 %form{:method => 'post', :action => "/jabber"}
