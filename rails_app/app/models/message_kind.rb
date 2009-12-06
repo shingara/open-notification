8eb1ab1006c53315b141ec15e14981926fe73c51
Cyril Mougel <cyril.mougel@gmail.com>
2009-12-07 00:16:42 +0100

    add support of mail notification in rails app

--- rails_app/app/models/message_kind.rb
+++ rails_app/app/models/message_kind.rb
@@ -1,7 +1,7 @@
 class MessageKind
   include MongoMapper::EmbeddedDocument
 
-  CHANNEL = ['jabber']
+  CHANNEL = ['jabber', 'mail']
 
   key :to, String, :required => true
   key :channel, String, :required => true
@@ -10,14 +10,16 @@ class MessageKind
 
   def send_notification
     if channel == "jabber"
-      notification('/jabber_notification/notif', _root_document.body)
+      notification('/jabber_notification/notif', _root_document.subject, _root_document.body)
+    elsif channel == 'mail'
+      notification('/mail_notification/notif', _root_document.subject, _root_document.body)
     else
       raise "We can't know this type. Need a nanite agent"
     end
   end
 
-  def notification(url, text)
-    Nanite.request(url, {'to' => self.to, 'body' => text}) { |response| update_send_at }
+  def notification(url, subject, text)
+    Nanite.request(url, {'to' => self.to, 'subject' => subject, 'body' => text}) { |response| update_send_at }
   end
 
   def update_send_at
